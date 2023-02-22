4e603d969d0a6f90896c545d030f4e07ba189fa3
Michael Dao <mike@turing.edu>
2023-02-22 14:23:15 -0700

    sets up forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/songs", to: "songs#index"
   get "/songs/:id", to: "songs#show"
   get "/artists/:artist_id/songs", to: "artist_songs#index"
+  get "/artists", to: "artists#index"
 end
