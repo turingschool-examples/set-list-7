d5fa8564bbfe54ec9689865567108c2c7dc3bbd9
Michael Dao <mike@turing.edu>
2023-02-22 16:26:53 -0700

    puts the binding.prys back in

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,7 @@ class Artist < ApplicationRecord
   has_many :songs
 
   def average_song_length
+    binding.pry
     songs.average(:length)
   end
 
@@ -14,6 +15,7 @@ class Artist < ApplicationRecord
   end
 
   def self.by_name
+    binding.pry
     order(:name)
   end
 end
