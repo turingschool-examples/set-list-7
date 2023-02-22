828ae2f5484b4e5556570bd64ac61788280bb7a6
Michael Dao <mike@turing.edu>
2023-02-21 18:31:23 -0700

    completes artist songs index

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ group :development, :test do
   gem "capybara"
   gem "launchy"
   gem "simplecov"
+  gem "shoulda-matchers", "~> 4.0"
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/songs", to: "songs#index"
   get "/songs/:id", to: "songs#show"
+  get "/artists/:artist_id/songs", to: "artist_songs#index"
 end
