d898b3da3617e6730c191f103da95b29dbfa6520
Michael Dao <mike@turing.edu>
2023-02-23 11:24:18 -0700

    fixes typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#indexget "/songs", to: "songs#index"
+  # root "articles#index
+  get "/songs", to: "songs#index"
   get "/songs/:id", to: "songs#show"
   get "/artists/:artist_id/songs", to: "artist_songs#index"
   get "/artists", to: "artists#index"
