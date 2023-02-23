02b37b1371e15676e6fba9d1c8cfc04a6465dc7b
Michael Dao <mike@turing.edu>
2023-02-22 18:19:36 -0700

    completes many to many

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_235224) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_010147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "playlist_songs", force: :cascade do |t|
+    t.bigint "song_id", null: false
+    t.bigint "playlist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["playlist_id"], name: "index_playlist_songs_on_playlist_id"
+    t.index ["song_id"], name: "index_playlist_songs_on_song_id"
+  end
+
+  create_table "playlists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -30,5 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_235224) do
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "playlist_songs", "playlists"
+  add_foreign_key "playlist_songs", "songs"
   add_foreign_key "songs", "artists"
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Song, type: :model do
-  it {should belong_to :artist}
+  it { should belong_to :artist }
+  it { should have_many :playlist_songs }
+  it { should have_many(:playlists).through(:playlist_songs) }
 end
