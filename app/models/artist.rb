47f960470fd578fd25459b728043cfb14bea487d
Chris Simmons <chris@turing.edu>
2023-05-10 12:00:05 -0400

    Completes associations-practice problems

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,9 +1,26 @@
 class Artist < ApplicationRecord
   has_many :songs
 
-  validates_presence_of :name
-
   def average_song_length
-    songs.average(:length)
+    self.songs.average(:length)
+  end
+
+# describe '#songs' do
+  # def songs
+  #   Song.where(artist_id: self.id)
+  # end
+
+# describe '#song_count'
+  def song_count
+    self.songs.count
+  end
+
+# describe '#wrote_song?' do
+  def wrote_song?(song)
+    # Version 1 WITH Assocations
+    self.songs.include?(song)
+
+    # Version 2 WITHOUT Associations
+    # song.artist_id == self.id
   end
 end
